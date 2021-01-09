36e93f720ccb37f0219b72a6d5e77d3df78385dc
Werner <werner.dohse@britehouse.co.za>
2021-01-09 11:00:51 +0200

    allow configurable token folder: add settings menu nad file directory choser. add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# v 0.1.2
+
+* Make sure to update the token and actor data after changing the art work.
+
+# v 0.1.1
+
+* Fix CR 1/2 token subfolder from cr0_50 ot cr0_5. 
+
+# v 0.1.0
+
+* Initial release
--- scripts/token-replacer.js
+++ scripts/token-replacer.js
@@ -1,18 +1,213 @@
-const tokenPath = "modules/token-replacer/tokens/";
+const tokenPathDefault = "modules/token-replacer/tokens/";
 const difficultyNameDefault = "cr";
 const difficultyVariableDefault = "data.details.cr";
+const BAD_DIRS = ["[data]", "[data] ", "", null];
 
 let cachedTokens = [];
 let replaceToken;
+let tokenDirectory;
 let difficultyName;
 let difficultyVariable;
 let hookedFromTokenCreation = false;
 
-/** direct hooks **/
+Hooks.on("renderTokenReplacerSetup", (app, html, user) => {
+    DirectoryPicker.processHtml(html);
+});
+
+// Token Replacer Setup Menu
+class TokenReplacerSetup extends FormApplication {
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.id = "token-replacer-settings";
+        options.template = "modules/token-replacer/handlebars/settings.handlebars";
+        options.width = 500;
+        return options;
+    }
+  
+    get title() { 
+         "Token Replacer Settings";
+    }
+  
+    /** @override */
+    async getData() {
+        const tokenDir = game.settings.get("token-replacer", "tokenDirectory");
+        const diffName = game.settings.get("token-replacer", "difficultyName");
+        const diffVariable = game.settings.get("token-replacer", "difficultyVariable");
+        
+        const dataDirSet = !BAD_DIRS.includes(tokenDir);
+
+        const setupConfig = {
+            "tokenDirectory": tokenDir,
+            "difficultyName": diffName,
+            "difficultyVariable": diffVariable,
+        };
+
+        let diffSpecified = true;
+        if (diffName !== "" && diffVariable === "") {
+            diffSpecified = false;
+        }
+        
+        const setupComplete = dataDirSet && diffSpecified;
+
+        return {
+            setupConfig: setupConfig,
+            setupComplete: setupComplete,
+        };
+    }
+  
+    /** @override */
+    async _updateObject(event, formData) {
+        event.preventDefault();
+
+        const tokenDir = formData['token-directory'];
+        const diffName = formData['difficulty-name'];
+        const diffVariable = formData['difficulty-variable'];
+
+        // if not difficulty name is specified then the variable is not needed
+        if (diffName === "") {
+            diffVariable = "";
+        }
+
+        await game.settings.set("token-replacer", "tokenDirectory", tokenDir);
+        await game.settings.set("token-replacer", "difficultyName", diffName);
+        await game.settings.set("token-replacer", "difficultyVariable", diffVariable);
+
+        const tokenDirSet = !BAD_DIRS.includes(tokenDir);
+
+        if (!tokenDirSet) {
+            $('#setup-feedback').text(`Please set the token directory to something other than the root.`);
+            $('#token-replacer-settings').css("height", "auto");
+            throw new Error(`Please set the token directory to something other than the root.`);
+        } else if (diffName !== "" && diffVariable === "") {
+            $('#setup-feedback').text(`If there is a 'Difficulty Name', you NEED to specify the 'Difficulty Variable'.`);
+            $('#ddb-importer-settings').css("height", "auto");
+            throw new Error(`If there is a 'Difficulty Name', you NEED to specify the 'Difficulty Variable'.`);
+        } else {
+            // recache the tokens
+            cacheAvailableTokens();            
+        }
+    }
+}
+
+class DirectoryPicker extends FilePicker {
+    constructor(options = {}) {
+      super(options);
+    }
+  
+    _onSubmit(event) {
+        event.preventDefault();
+        const path = event.target.target.value;
+        const activeSource = this.activeSource;
+        const bucket = event.target.bucket ? event.target.bucket.value : null;
+        this.field.value = DirectoryPicker.format({
+        activeSource,
+        bucket,
+        path,
+        });
+        this.close();
+    }
+  
+    static async uploadToPath(path, file) {
+        const options = DirectoryPicker.parse(path);
+        return FilePicker.upload(options.activeSource, options.current, file, { bucket: options.bucket });
+    }
+  
+    // returns the type "Directory" for rendering the SettingsConfig
+    static Directory(val) {
+        return val;
+    }
+  
+    // formats the data into a string for saving it as a GameSetting
+    static format(value) {
+        return value.bucket !== null
+        ? `[${value.activeSource}:${value.bucket}] ${value.path}`
+        : `[${value.activeSource}] ${value.path}`;
+    }
+  
+    // parses the string back to something the FilePicker can understand as an option
+    static parse(str) {
+        let matches = str.match(/\[(.+)\]\s*(.+)/);
+        if (matches) {
+        let source = matches[1];
+        const current = matches[2].trim();
+        const [s3, bucket] = source.split(":");
+        if (bucket !== undefined) {
+            return {
+            activeSource: s3,
+            bucket: bucket,
+            current: current,
+            };
+        } else {
+            return {
+            activeSource: s3,
+            bucket: null,
+            current: current,
+            };
+        }
+        }
+        // failsave, try it at least
+        return {
+        activeSource: "data",
+        bucket: null,
+        current: str,
+        };
+    }
+  
+    // Adds a FilePicker-Simulator-Button next to the input fields
+    static processHtml(html) {
+        $(html)
+        .find(`input[data-dtype="Directory"]`)
+        .each((index, element) => {
+            // disable the input field raw editing
+            $(element).prop("readonly", true);
+
+            // if there is no button next to this input element yet, we add it
+            if (!$(element).next().length) {
+            let picker = new DirectoryPicker({
+                field: $(element)[0],
+                ...DirectoryPicker.parse($(element).val()),
+            });
+            let pickerButton = $(
+                '<button type="button" class="file-picker" data-type="imagevideo" data-target="img" title="Pick directory"><i class="fas fa-file-import fa-fw"></i></button>'
+            );
+            pickerButton.on("click", () => {
+                picker.render(true);
+            });
+            $(element).parent().append(pickerButton);
+            }
+        });
+    }
+  
+    /** @override */
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        // remove unnecessary elements
+        $(html).find("ol.files-list").remove();
+        $(html).find("footer div").remove();
+        $(html).find("footer button").text("Select Directory");
+    }
+}
+  
+Hooks.on("renderSettingsConfig", (app, html, user) => {
+    DirectoryPicker.processHtml(html);
+});
+
 // initialisation
-Hooks.on("init", () => {
+function init() {
     console.log("Token Replacer initialising...");
-    
+
+    // hook up events
+    hookupEvents();
+};
+
+// module ready
+function ready() {
+    // cache the tokens
+    cacheAvailableTokens();
+};
+
+function registerSettings() {
     // token replacement setting
     game.settings.register("token-replacer", "replaceToken", {
         name: "Automatically replace NPC actor artwork",
@@ -29,16 +224,35 @@ Hooks.on("init", () => {
             3: "Replace token & portrait artwork",
         },
         default: 0,
-    }); 
+    });
+    
+    game.settings.registerMenu("token-replacer", 'setupMenu', {
+        name: "Settings",
+        label: "Settings",
+        hint: "Token directory settings",
+        icon: 'fas fa-wrench',
+        type: TokenReplacerSetup,
+        restricted: true
+      });
 
-    // token path setting
+    // token directory
+    game.settings.register("token-replacer", "tokenDirectory", {
+        name: "Token Directory",
+        hint: "The directory the tokens are found in. Defaults to the free Forgotten Adventures tokens. It's however advised to change this directory to something NOT in the modules directory as new versions will overwrite any tokens you save there.",
+        scope: "world",
+        config: false,
+        type: DirectoryPicker.Directory,
+        default: `[data] ${tokenPathDefault}`,
+    });
+
+    // token subdirectory path setting
     game.settings.register("token-replacer", "difficultyName", {
         name: "NPC Difficulty Name",
         hint: "The difficulty measurement name used in your system (defaults to 'cr' for DnD 5e). " + 
             "Token art should be put under folders named after this measurement and is case sensitive (the cr1 subfolder should have all cr1 tokens). " + 
             "Leaving this empty will mean you need to save the images in the root of the tokens path and not in subfolders and the 'NPC Difficulty Variable' is not checked to figure out subfolder structures.",
         scope: "world",
-        config: true,
+        config: false,
         type: String,
         default: difficultyNameDefault,
     });
@@ -48,39 +262,40 @@ Hooks.on("init", () => {
         hint: "The difficulty measurement variable used in the actor's data (defaults to DnD 5e's). This is dependant on whether you entered an 'NPC Difficulty Name'. " + 
             "If you are unsure what the variable is, leave 'NPC Difficulty Name' empty and save everyting into the root folder.",
         scope: "world",
-        config: true,
+        config: false,
         type: String,
         default: difficultyVariableDefault,
     });
-
-    // hook up events
-    hookupEvents();
-});
-
-// module ready
-Hooks.on("ready", () => {
-    // cache the tokens
-    cacheAvailableTokens();
-});
-
+}
 
 /** handle functions **/
 // grab the already saved settings
 function grabSavedSettings() {
     replaceToken = game.settings.get("token-replacer", "replaceToken") ?? 0;
+    if (game.settings.get("token-replacer", "tokenDirectory") === "[data] " || "") {
+        tokenDirectory = { 
+            activeSource: "data",
+            current: tokenPathDefault,
+        }
+    } else {
+        tokenDirectory = DirectoryPicker.parse(game.settings.get("token-replacer", "tokenDirectory"))
+    }    
     difficultyName = game.settings.get("token-replacer", "difficultyName");
     difficultyVariable = game.settings.get("token-replacer", "difficultyVariable");
 }
 
 // cache the set of available tokens which can be used to replace artwork to avoid repeated filesystem requests
 async function cacheAvailableTokens() {
+    // grab the saved values
+    grabSavedSettings();
+
     cachedTokens = [];
     
     // any files in the root (maybe they didn't want to use subfolders)
-    const rootTokens = await FilePicker.browse("data", tokenPath);
+    const rootTokens = await FilePicker.browse(tokenDirectory.activeSource, tokenDirectory.current);
     rootTokens.files.forEach(t => cachedTokens.push(t));
 
-    const folders = await FilePicker.browse("data", tokenPath);
+    const folders = await FilePicker.browse(tokenDirectory.activeSource, tokenDirectory.current);
     // any files in subfolders
 	for ( let folder of folders.dirs ) {
 		const tokens = await FilePicker.browse("data", folder);
@@ -105,8 +320,7 @@ function preCreateActorHook(data, options, userId) {
     hookedFromTokenCreation = false;
 
     if ( !replaceToken || (data.type !== "npc") || !hasProperty(data, difficultyVariable) ) return;
-    replaceArtWork(data);
-    data.update(data);
+    replaceArtWork(data);    
 }
 
 // handle createActor hook
@@ -136,11 +350,11 @@ function preCreateTokenHook(data, options, userId) {
 
 // replace the artwork for a NPC actor with the version from this module
 function replaceArtWork(data) {
-    const formattedName = data.name.replace(/ /g, "_");
+    const formattedName = escape(data.name.replace(/ /g, "_"));
 	const diffDir = (difficultyName) ? `${String(getProperty(data, difficultyVariable)).replace(".", "_")}/` : "";
-    const tokenCheck = `${tokenPath}${difficultyName}${diffDir}${formattedName}`;
+    const tokenCheck = `${tokenDirectory.current}/${difficultyName}${diffDir}${formattedName}`;
     
-    const filteredCachedTokens = cachedTokens.filter(t => t.indexOf(tokenCheck) >= 0);
+    const filteredCachedTokens = cachedTokens.filter(t => t.toLowerCase().indexOf(tokenCheck.toLowerCase()) >= 0);
     if (!filteredCachedTokens || (filteredCachedTokens && !filteredCachedTokens.length)) return;
 
     if (filteredCachedTokens && filteredCachedTokens.length) {
@@ -162,3 +376,7 @@ function replaceArtWork(data) {
     
     return data;
 }
+
+Hooks.once("init", init);
+Hooks.once("ready", registerSettings);
+Hooks.on("ready", ready);
